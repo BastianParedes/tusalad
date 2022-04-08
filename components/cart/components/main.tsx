248f48ffdbeb27db60cc7c9836d2217a99b8bef2
Bastián <bastian.p@outlook.com>
2022-04-08 11:56:47 -0400

    Avanzado los productos

--- components/cart/components/main.tsx
+++ components/cart/components/main.tsx
@@ -1,10 +1,13 @@
-import styles from '../styles/main.module.css';
 
+import { BsArrowReturnLeft } from 'react-icons/bs';
+import styles from '../styles/main.module.css';
+import Products from './products';
 
 export default function Main(props: any) {
     return (
         <main className={styles['main']}>
-
+            <a className={styles['']} href='/'><BsArrowReturnLeft /> Volver a la página principal</a>
+            <Products />
         </main>
     );
 }
--- components/cart/components/products.tsx
+++ components/cart/components/products.tsx
@@ -1,18 +1,54 @@
+
+import React from 'react';
+
 import styles from '../styles/products.module.css';
-
-
 
 const JSONProducts: any = require('/public/products.json');
 
-function Product(props: any) {
-
+type ProductProps = {
+    productKey: string,
+    quantity: number,
 }
 
 
-export default function Products(props: any) {
+function Product(props: ProductProps) {
+    let [quantity, setQuantity]: any = React.useState(props.quantity);
+
+    React.useEffect((): void => {
+        let cart = JSON.parse(sessionStorage.getItem('cart') || '{}');
+        cart[props.productKey] = quantity;
+        sessionStorage.setItem('cart', JSON.stringify(cart));
+    }, [quantity]);
+
+    const productInfo: {name: string, price: number, src: string, description: string, included: string} = JSONProducts[props.productKey];
+    return (
+        <div className={styles['product-container']}>
+            <img className={styles['product-image']} src="" alt="" />
+            <div className={styles['product-info-container']}>
+                <span className={styles['product-name']}>{productInfo.name}</span>
+                <span className={styles['']}>Descripción</span>
+            </div>
+            <div className={styles['']}>
+                <button className={styles['button-add-one']} onClick={(): void => setQuantity((q: number): number => Math.max(0, q-1))}>-</button>
+                <input type="number" className={styles['']} value={quantity} onChange={(event: any): void => setQuantity(Math.max(0, parseInt(event.target.value)))} />
+                <button className={styles['button-substract-one']} onClick={(): void => setQuantity((q: number): number => Math.max(0, q+1))}>+</button>
+            </div>
+        </div>
+    );
+}
+
+
+export default function Products() {
+    let [cart, setCart]: any[] = React.useState({});
+
+    React.useEffect((): void => {
+        setCart(() => JSON.parse(sessionStorage.getItem('cart') || '{}'));
+    }, [])
+    
+
     return (
         <div className={styles['products']}>
-
+            {Object.keys(cart).map((key: string) => <Product key={key} productKey={key} quantity={cart[key]} />)}
         </div>
     );
 }
