9df337ddc77c59fd55ccbd09721c901e8b460f79
Bastián <bastian.p@outlook.com>
2022-05-03 22:10:53 -0400

    Cambiado el solgan y la descripción

--- components/index/about/index.tsx
+++ components/index/about/index.tsx
@@ -5,8 +5,8 @@ import Section from '../section';
 export default function About() {
     return (
         <Section id='about' tittle='Sobre nosotros'>
-            <h3 className={styles['slogan']}>Insertar slogan aquí</h3>
-            <p className={styles['content']}>Tu Salad es un emprendimiento bla bla bla bla Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum velit dicta, amet illum saepe adipisci incidunt eius. Sed, obcaecati delectus tempora fuga accusantium mollitia ratione, amet, vitae voluptates corporis reiciendis?</p>
+            <h3 className={styles['slogan']}>Porque lo saludable sabe bien</h3>
+            <p className={styles['content']}>Tu Salad es un emprendimiento dedicado a la venta de bowls de comida sana como ninguna otra tienda puede hacerlo. Usamos ingredientes de máxima calidad y recetas propias que promueven la vida sana sin dejar de lado su rico sabor. Compra con nosotros y nos aseguraremos de que tu compra llegue a la puerta de tu casa mintos mas tarde.</p>
         </Section>
     );
 }
